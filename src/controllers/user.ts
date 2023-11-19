eb3edcb9f1c3a15e352468d2f56f07f4bc72ed0b
gbenba <gbenba13@gmail.com>
2023-11-19 20:06:13 -0300

    first commit

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,7 +3,11 @@ import UserModel from '../models/user';
 
 abstract class UserController {
 	static async createNew(req: Request, res: Response) {
-		// VALIDAR DATOS CON ZOD
+		// VALIDAR DATOS CON ZOD:
+			// llamar a ZOD, crear los esquemas, mandarle el req.body a zod, 
+			// que haga la manipulacion de datos y que me entregue el objeto procesa 
+			// y validado para pasarselo al modelo
+		
 		const validatedData = req.body;
 
 		console.log(req.body);
